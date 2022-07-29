7440c3093c3a44c686c36d76794ee62b9c6ac62d
Brayan Henao Raigosa <bhenao@intergrupo.com>
2022-07-28 19:03:22 -0500

    restaurants avance diseño

--- lib/ui/pages/restaurants/restaurants_page.dart
+++ lib/ui/pages/restaurants/restaurants_page.dart
@@ -39,7 +39,83 @@ class RestaurantsPage extends StatelessWidget {
                   searchHandler: (text) => {},
                   hintText: 'What do you want to eat?',
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            offset: Offset(-2, 2),
+                            blurRadius: 5,
+                            color: Colors.black26,
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.filter_list,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 40,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) => Row(
+                            children: [
+                              const SizedBox(width: 10),
+                              Container(
+                                padding: const EdgeInsets.only(
+                                  left: 12,
+                                  top: 5,
+                                  bottom: 5,
+                                  right: 2,
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey[200]!,
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: Colors.white,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: const [
+                                    Text(
+                                      'Ordenar',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_drop_down,
+                                      color: Colors.black,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          itemCount: 30,
+                          scrollDirection: Axis.horizontal,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
